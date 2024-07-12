90bad3ef41666f7be69a9d1cbbac8139bdb7d28c
Aithan Eulysse Gimenez <gimenezaithan@gmail.com>
2024-07-12 10:34:32 +0800

    added login/register ui

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
